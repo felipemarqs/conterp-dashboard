ef0af3179c66b6bcfc4294c63f402c2565092264
Felipe Marques <felipe.marqs@outlook.com>
2023-02-05 18:40:40 -0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import helmet from 'helmet';
+import dotenv from 'dotenv'
