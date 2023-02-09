91c10f9db482b10fc568b8a2290d584397edebd2
Felipe Marques <felipe.marqs@outlook.com>
2023-02-08 21:09:21 -0300

    feat: :sparkles: att

--- client/src/scenes/dashboard/index.jsx
+++ client/src/scenes/dashboard/index.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 
+import { useGetContractsQuery } from 'state/api'
+
+
+
+
 const Dashboard = () => {
+
+  //console.log(process.env.REACT_APP_BASE_URL)
+  const {data} = useGetContractsQuery()
+  console.log(data)
+
   return (
     <div>Dashboard</div>
   )
--- server/index.js
+++ server/index.js
@@ -5,17 +5,17 @@ import mongoose from "mongoose";
 import helmet from "helmet";
 import dotenv from "dotenv";
 import morgan from "morgan";
-import xlsx from 'xlsx'
+import xlsx from "xlsx";
 
 //Models
 import Contract from "./models/Contract.js";
 import Vehicle from "./models/Vehicle.js";
-import Refuel from './models/Refuel.js';
+import Refuel from "./models/Refuel.js";
 
 //Data
-import {contractData} from "./data/index.js";
+import { contractData } from "./data/index.js";
 
-import generalRoutes from "./routes/general.js";
+import contractRoutes from "./routes/contract.js";
 
 /*Configurando o servidor*/
 
@@ -30,11 +30,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /* Rotas */
-app.use("/contract", generalRoutes);
+app.use("/contract", contractRoutes);
+//app.use("/vehicle", vehicleRoutes);
 
 /* Configurando o Mongoose */
 
-const PORT = process.env.PORT || 9000
+const PORT = process.env.PORT || 9000;
 
 //Importing data from Excel
 let workbook = xlsx.readFile("./data/excelFiles/refuel.xlsx");
@@ -42,97 +43,78 @@ let workbook = xlsx.readFile("./data/excelFiles/refuel.xlsx");
 let worksheet = workbook.Sheets[workbook.SheetNames[0]];
 
 const insertVehicle = async () => {
-
-  const contract = await Contract.find({name: "Integridade"})
+  const contract = await Contract.find({ name: "Integridade" });
   const contractId = contract[0]._id;
 
-  await Vehicle.create(
-    {
-      contractId: contractId,
-      contract: "Integridade",
-      plate: "RTV7A56",
-      type: "Automóvel",
-      manufacturer: "FIAT",
-      model: "STRADA",
-      color: 'BRANCA',
-      tankCapacity: 55,
-      year: 2022,
-      isActive: true
-    }
-  )
-  
-  const newVehicleDb = await Vehicle.find({ plate : "RTV7A56"})
-  console.log ("novo carro:" , newVehicleDb)
-  
-  contract[0].vehicles.push(newVehicleDb[0]._id)
+  await Vehicle.create({
+    contractId: contractId,
+    contract: "Integridade",
+    plate: "RTV7A56",
+    type: "Automóvel",
+    manufacturer: "FIAT",
+    model: "STRADA",
+    color: "BRANCA",
+    tankCapacity: 55,
+    year: 2022,
+    isActive: true,
+  });
 
+  const newVehicleDb = await Vehicle.find({ plate: "RTV7A56" });
+  console.log("novo carro:", newVehicleDb);
 
-  await contract[0].save()
-}
+  contract[0].vehicles.push(newVehicleDb[0]._id);
+
+  await contract[0].save();
+};
 
 const insertRefuelData = async () => {
   const refuelData = [];
 
   function ExcelDateToJSDate(date) {
-    return new Date(Math.round((date - 25569)*86400*1000));
+    return new Date(Math.round((date - 25569) * 86400 * 1000));
   }
 
   for (let i = 2; i <= 15042; i++) {
-
-    console.log("Veículo atual", i - 1)
+    console.log("Veículo atual", i - 1);
 
     const plate = worksheet["A" + i].v;
     const date = worksheet["B" + i].v;
     const quantity = worksheet["C" + i].v;
     const price = worksheet["D" + i].v;
     const fuelType = worksheet["E" + i].v;
-    
-  
-   console.log(plate)
-  
-    const vehicle = await Vehicle.find({ plate : plate})
-    
+
+    console.log(plate);
+
+    const vehicle = await Vehicle.find({ plate: plate });
+
     const vehicleId = vehicle[0]._id;
-  
-    refuelData.push(
-      {
-        plate: plate,
-        date: ExcelDateToJSDate(date),
-        vehicle: vehicleId,
-        quantity: quantity,
-        price: price,
-        fuelType: fuelType,
-      }
-    )
-   
-  
-    }
 
-  Refuel.insertMany(refuelData)
-    
-   
+    refuelData.push({
+      plate: plate,
+      date: ExcelDateToJSDate(date),
+      vehicle: vehicleId,
+      quantity: quantity,
+      price: price,
+      fuelType: fuelType,
+    });
+  }
 
-  
-}
-
-
-
+  Refuel.insertMany(refuelData);
+};
 
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }) 
+  })
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`Server running in port ${PORT}`)
+      console.log(`Server running in port ${PORT}`);
       //Insert data to Database (ONLY RUN IT ONCE!!)
       //Contract.insertMany(contractData)
 
-      insertRefuelData();
-     //insertVehicle()
-      
+      //insertRefuelData();
+      //insertVehicle()
     });
-
   })
   .catch((error) => console.log(`${error} not connected`));
