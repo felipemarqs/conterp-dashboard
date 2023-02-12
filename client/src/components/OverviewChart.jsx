ed9a8044497c27e50dd690f3b59d4ca3d1dc3905
Felipe Marques <felipe.marqs@outlook.com>
2023-02-11 21:03:44 -0300

    Update OverviewChart.jsx

--- client/src/components/OverviewChart.jsx
+++ client/src/components/OverviewChart.jsx
@@ -6,18 +6,37 @@ import { useGetRefuelQuery } from "../state/api.js";
 
 const OverviewChart = ({ isDashboard = false, view }) => {
   const { data, isLoading } = useGetRefuelQuery();
-  console.log("data", data);
+  //console.log("data", data);
 
   const theme = useTheme();
 
-  const map = data.map((element) => {
-    console.log("element", element.date);
-    let monthData = new Date(element.date)
+  if (data) {
+    const obj = {...data}
+    //console.log("obj",obj );
+      const map = data.map((element) => {
+      //console.log("element", {...element});
+      //console.log("element", element.date);
+      let monthData = new Date(element.date)
+  
+  
+     let month = monthData.toLocaleString("default" , { month: 'long'})
+      //console.log("month",month)
+      return {
+        month, ...element
+      }
+    })
 
+    const redux = Object.values(map).reduce((acc, key) => {
+      console.log("acc", acc)
+      console.log("key", key)
 
-    let month = monthData.toLocaleString("default" , { month: 'long'})
-    console.log("month",month)
-  })
+      const jan = acc.month === "janeiro" ? acc.quantity + key.quantity : undefined
+    })
+
+    console.log("map",map)
+    //console.log("redux",redux)
+  }
+  
 
   
   return <div>Overview Chart</div>;
